863f6260e400ef36172c7a7b809841d6497884b9
nikhil <nvemuri4649@gmail.com>
2025-07-07 15:34:08 -0400

    aynchio-ed paper qa2

--- paperqa2test.py
+++ paperqa2test.py
@@ -1,13 +1,18 @@
+import asyncio
 from dotenv import load_dotenv
 
 load_dotenv()
 
-from paperqa import Settings, ask
+from paperqa import Settings, Docs, ask
+
+async def main():
+    docs = Docs()
+    await docs.aadd("sample_papers_machinelearning/ART20203995.pdf")
+    settings = Settings()
+    settings.llm = "gpt-3.5-turbo"
+    ask = input("Ask a machine learning question: ")
+    answer_response = await docs.aquery(ask, settings=settings)
+    print(answer_response)
 
 if __name__ == "__main__":
-    query = input("Ask a machine learning question: ")
-    answer_response = ask(
-        query,
-        settings=Settings(temperature=0.5, paper_directory="sample_papers_machinelearning/"),
-    )
-    #print(answer_response)
+    asyncio.run(main())
