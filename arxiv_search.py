b0d1ed281f9966002136c7d66442ac010c1530e4
Sutharsika Kumar <sutharsika.kumar07@gmail.com>
2025-07-18 01:45:34 -0400

    langgraph basic structure

---
+++ arxiv_search.py
@@ -0,0 +1,5 @@
+
+def search_arxiv(query, max_results=25):
+
+def summarize_pdf(paper):
+
---
+++ langgraph_pipeline.py
@@ -0,0 +1,18 @@
+from langchain_openai import ChatOpenAI
+from langchain_community.embeddings import OpenAIEmbeddings
+from langgraph import Graph, node, Tool, ExternalNode
+
+
+def fetch_arxiv(state):
+
+def summarise_each(state):
+    
+def extract_limits(state):
+
+def upsert(state):
+
+def future_work(state):
+
+def user_choice(state, event):
+
+def project_future_work(state):
